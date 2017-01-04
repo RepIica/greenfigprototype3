542e150c5810411a4c93fa5918a58d607dd8bb44
RepIica <ketrixz@gmail.com>
2017-01-04 16:31:23 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		navigation : false,
+		slideSpeed : 300,
+		paginationSpeed: 400,
+		singleItem: true,
+		autoPlay : true,
+		stopOnHover : true,
+		pagination : false
+	});
+});
